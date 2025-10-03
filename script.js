2bad0ef9e076818c9af75773ed844b0fec1734fb
joao gabriel pereira couto <joaocouto114@gmail.com>
2025-10-03 17:56:30 -0300

    adicionando interatividade com teclado numerico

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-        var res = document.getElementById("res")
+       // recebendo os elementos do html
+       var res = document.getElementById("res")
         var visor = document.getElementById("tn1")
         var soma = document.getElementById('somar')
         var subtracao = document.getElementById('subtrair')
@@ -8,71 +9,118 @@
         var limpa = document.getElementById('limpar')
         var igual = document.getElementById('calcular')
         var apaga = document.getElementById('apagar')
+        var ponto = document.getElementById('ponto')
 
+
+        // declaraçao de ariaveis globais
         var num1 = null
         var operador = null
         var num2 = null
         var resultado = null
 
+
+
+        // adicionando eventlistenes aos elementos html
         soma.addEventListener('click', () => salvarnumero('+'))
         subtracao.addEventListener('click', () => salvarnumero('-'))
         divisao.addEventListener('click', () => salvarnumero('/'))
         mutiplicacao.addEventListener('click', () => salvarnumero('*'))
+        
+        //permite que seja usado o teclado numerico para calcular
+        document.addEventListener('keydown', function(event){
+            if (/[0-9.]/.test(event.key)) addnumber(event.key);
+            if (['+', '-', '*', '/'].includes(event.key)) salvarnumero(event.key);
+            if (event.key === 'Enter') calcular();
+            if (event.key === 'Backspace') apagar();
+        });
+
+        //percorre o array da classe numeros para adicionar um eventlistener em cada elemento
         for (let btn of numero) {
             btn.addEventListener('click', () => addnumber(btn.value))
         }
         limpa.addEventListener('click', () => limpar())
         apaga.addEventListener('click', () => apagar())
         igual.addEventListener('click', () => calcular())
-
-       function addnumber(number){
-        visor.value += number
         
+        //trata a entrada de dados para que o usuario nao possa enviar letras
+        visor.addEventListener("keypress", function(event) {
+            const char = event.key
+            if (!/[0-9.]/.test(char)) {
+                event.preventDefault() 
+            }
+        })
+
+       //inicio da sessao de funçoes
+       //funçao para adicionar um numero no visor
+       function addnumber(number){
+        //verifica se o usuario esta tentando adicionar 2 pontos no visor e caso positivo nega ao usuario
+        if (number === "." && visor.value.includes(".")){
+            return
+        }
+        // adiciona o numero ao visor
+        visor.value += number
        }
+
+
+        // funçao para apagar(backspace)
         function apagar(){
             visor.value = visor.value.slice(0, -1)
         }
 
+
+       //funçao para limpar todos os dados e recomeçar os cauculos
        function limpar(){
         visor.value = ""
-        res.innerHTML = `resutado:`
+        res.innerHTML = `resultado:`
         num1 = null
         num2 = null
         operador = null
        }
-       function salvarnumero(op){
-        if (visor.value !== "") {
-            if (num1 === null) {
-                num1 = visor.value
-            } else {
-                calcular() 
-            }
-        }
-            operador = op
-            visor.value = ""
-       }
-        function calcular(){
-            var n1 = Number(num1)
-            var n2 = Number(visor.value)  
-            if (operador==='-'){
-                resultado = n1 - n2
-            }
-            if (operador==='+'){
-                resultado = n1 + n2
 
+
+      //funçao para salvar o numero que esta no visor
+      function salvarnumero(op){
+        //garante que o usuario nao consiga salvar o "vazio"
+        if (visor.value === "") return;
+        //verifica se é o primeiro calculo ou se esta utilizando o resultado anterior para um novo calculo 
+        if (num1 === null) {
+            num1 = Number(visor.value); 
+        } else {
+            calcular(); 
+        }
+        //recebe o operador a ser utilizado e apaga os numeros do visor
+        operador = op;
+        visor.value = ""; 
+    }
+
+
+
+        //funçao de calculo
+        function calcular(){
+            //garante que nenhum dos parametros estejam vazios
+            if (num1 === null|| visor.value === ""|| operador === null) return
+
+            //atribui o valor salvo em num1 para n1 e o valor do visor para n2
+            var n1 = Number(num1)
+            var n2 = Number(visor.value)
+
+
+            // verifica ao operador, faz o calculo e atribui a var resultaddo
+            switch(operador){
+                case '+' : resultado = n1 + n2; break
+                case '-' : resultado = n1 - n2; break
+                case '*' : resultado = n1 * n2; break
+                case '/' : 
+                    if (n2 === 0){
+                        res.innerHTML = `nao é possivel dividir por 0`
+                    }else{
+                       resultado = n1 / n2 
+                    }; break
+                
             }
-            if (operador==='*'){
-                resultado = n1 * n2
-            }
-            if (operador==='/'){
-                if (n2===0){
-                    res.innerHTML = `nao é possivel dividir por zero`
-                    return
-                }else{
-                    resultado = n1 / n2
-                }
-            }
-            res.innerHTML = `resltado: ${resultado}`
+            //mostra o resultado e redefine os parametros
+            res.innerHTML = `resultado: ${resultado}`
             visor.value = resultado
             num1 = resultado
+            operador = null
         }
